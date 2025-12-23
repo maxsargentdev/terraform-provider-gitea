d5fb8dead543b1f5c7296d8684f35bca27f6e685
SargentM <Max.Sargent@starlizard.com>
2025-12-23 15:07:31 +0000

    Use the mapping function for all resources / data_sources

--- internal/provider/branch_protection_data_source.go
+++ internal/provider/branch_protection_data_source.go
@@ -71,32 +71,38 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		return
 	}
 
-	// Map response to data - use same mapping function as resource
-	data.BranchName = types.StringValue(protection.BranchName)
-	data.RuleName = types.StringValue(protection.RuleName)
-	data.Name = types.StringValue(protection.BranchName)
-	data.EnablePush = types.BoolValue(protection.EnablePush)
-	data.EnablePushWhitelist = types.BoolValue(protection.EnablePushWhitelist)
-	data.EnableMergeWhitelist = types.BoolValue(protection.EnableMergeWhitelist)
-	data.EnableStatusCheck = types.BoolValue(protection.EnableStatusCheck)
-	data.RequireSignedCommits = types.BoolValue(protection.RequireSignedCommits)
-	data.CreatedAt = types.StringValue(protection.Created.String())
-	data.UpdatedAt = types.StringValue(protection.Updated.String())
+	mapBranchProtectionToDataSourceModel(ctx, protection, &data)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+// Helper function to map Gitea BranchProtection to data source model
+func mapBranchProtectionToDataSourceModel(ctx context.Context, protection *gitea.BranchProtection, model *datasource_branch_protection.BranchProtectionModel) {
+	// Note: owner, repo, and branch_name need to be preserved from config (not overwritten from API)
+	model.RuleName = types.StringValue(protection.RuleName)
+	model.Name = types.StringValue(protection.BranchName)
+	model.EnablePush = types.BoolValue(protection.EnablePush)
+	model.EnablePushWhitelist = types.BoolValue(protection.EnablePushWhitelist)
+	model.EnableMergeWhitelist = types.BoolValue(protection.EnableMergeWhitelist)
+	model.EnableStatusCheck = types.BoolValue(protection.EnableStatusCheck)
+	model.RequireSignedCommits = types.BoolValue(protection.RequireSignedCommits)
+	model.CreatedAt = types.StringValue(protection.Created.String())
+	model.UpdatedAt = types.StringValue(protection.Updated.String())
 
 	// Map boolean fields from API
-	data.PushWhitelistDeployKeys = types.BoolValue(protection.PushWhitelistDeployKeys)
-	data.BlockOnRejectedReviews = types.BoolValue(protection.BlockOnRejectedReviews)
-	data.BlockOnOfficialReviewRequests = types.BoolValue(protection.BlockOnOfficialReviewRequests)
-	data.BlockOnOutdatedBranch = types.BoolValue(protection.BlockOnOutdatedBranch)
-	data.DismissStaleApprovals = types.BoolValue(protection.DismissStaleApprovals)
-	data.EnableApprovalsWhitelist = types.BoolValue(protection.EnableApprovalsWhitelist)
+	model.PushWhitelistDeployKeys = types.BoolValue(protection.PushWhitelistDeployKeys)
+	model.BlockOnRejectedReviews = types.BoolValue(protection.BlockOnRejectedReviews)
+	model.BlockOnOfficialReviewRequests = types.BoolValue(protection.BlockOnOfficialReviewRequests)
+	model.BlockOnOutdatedBranch = types.BoolValue(protection.BlockOnOutdatedBranch)
+	model.DismissStaleApprovals = types.BoolValue(protection.DismissStaleApprovals)
+	model.EnableApprovalsWhitelist = types.BoolValue(protection.EnableApprovalsWhitelist)
 
 	// Map integer fields
-	data.RequiredApprovals = types.Int64Value(protection.RequiredApprovals)
+	model.RequiredApprovals = types.Int64Value(protection.RequiredApprovals)
 
 	// Map string fields
-	data.ProtectedFilePatterns = types.StringValue(protection.ProtectedFilePatterns)
-	data.UnprotectedFilePatterns = types.StringValue(protection.UnprotectedFilePatterns)
+	model.ProtectedFilePatterns = types.StringValue(protection.ProtectedFilePatterns)
+	model.UnprotectedFilePatterns = types.StringValue(protection.UnprotectedFilePatterns)
 
 	// Map list fields
 	if len(protection.PushWhitelistUsernames) > 0 {
@@ -104,9 +110,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.PushWhitelistUsernames {
 			elements[i] = types.StringValue(v)
 		}
-		data.PushWhitelistUsernames, _ = types.ListValue(types.StringType, elements)
+		model.PushWhitelistUsernames, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.PushWhitelistUsernames = types.ListNull(types.StringType)
+		model.PushWhitelistUsernames = types.ListNull(types.StringType)
 	}
 
 	if len(protection.PushWhitelistTeams) > 0 {
@@ -114,9 +120,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.PushWhitelistTeams {
 			elements[i] = types.StringValue(v)
 		}
-		data.PushWhitelistTeams, _ = types.ListValue(types.StringType, elements)
+		model.PushWhitelistTeams, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.PushWhitelistTeams = types.ListNull(types.StringType)
+		model.PushWhitelistTeams = types.ListNull(types.StringType)
 	}
 
 	if len(protection.MergeWhitelistUsernames) > 0 {
@@ -124,9 +130,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.MergeWhitelistUsernames {
 			elements[i] = types.StringValue(v)
 		}
-		data.MergeWhitelistUsernames, _ = types.ListValue(types.StringType, elements)
+		model.MergeWhitelistUsernames, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.MergeWhitelistUsernames = types.ListNull(types.StringType)
+		model.MergeWhitelistUsernames = types.ListNull(types.StringType)
 	}
 
 	if len(protection.MergeWhitelistTeams) > 0 {
@@ -134,9 +140,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.MergeWhitelistTeams {
 			elements[i] = types.StringValue(v)
 		}
-		data.MergeWhitelistTeams, _ = types.ListValue(types.StringType, elements)
+		model.MergeWhitelistTeams, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.MergeWhitelistTeams = types.ListNull(types.StringType)
+		model.MergeWhitelistTeams = types.ListNull(types.StringType)
 	}
 
 	if len(protection.ApprovalsWhitelistUsernames) > 0 {
@@ -144,9 +150,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.ApprovalsWhitelistUsernames {
 			elements[i] = types.StringValue(v)
 		}
-		data.ApprovalsWhitelistUsername, _ = types.ListValue(types.StringType, elements)
+		model.ApprovalsWhitelistUsername, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.ApprovalsWhitelistUsername = types.ListNull(types.StringType)
+		model.ApprovalsWhitelistUsername = types.ListNull(types.StringType)
 	}
 
 	if len(protection.ApprovalsWhitelistTeams) > 0 {
@@ -154,9 +160,9 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.ApprovalsWhitelistTeams {
 			elements[i] = types.StringValue(v)
 		}
-		data.ApprovalsWhitelistTeams, _ = types.ListValue(types.StringType, elements)
+		model.ApprovalsWhitelistTeams, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.ApprovalsWhitelistTeams = types.ListNull(types.StringType)
+		model.ApprovalsWhitelistTeams = types.ListNull(types.StringType)
 	}
 
 	if len(protection.StatusCheckContexts) > 0 {
@@ -164,20 +170,18 @@ func (d *branchProtectionDataSource) Read(ctx context.Context, req datasource.Re
 		for i, v := range protection.StatusCheckContexts {
 			elements[i] = types.StringValue(v)
 		}
-		data.StatusCheckContexts, _ = types.ListValue(types.StringType, elements)
+		model.StatusCheckContexts, _ = types.ListValue(types.StringType, elements)
 	} else {
-		data.StatusCheckContexts = types.ListNull(types.StringType)
+		model.StatusCheckContexts = types.ListNull(types.StringType)
 	}
 
 	// Set remaining fields as null (not returned by API)
-	data.ForcePushAllowlistUsernames = types.ListNull(types.StringType)
-	data.ForcePushAllowlistTeams = types.ListNull(types.StringType)
-	data.BlockAdminMergeOverride = types.BoolNull()
-	data.EnableForcePush = types.BoolNull()
-	data.EnableForcePushAllowlist = types.BoolNull()
-	data.ForcePushAllowlistDeployKeys = types.BoolNull()
-	data.IgnoreStaleApprovals = types.BoolNull()
-	data.Priority = types.Int64Null()
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	model.ForcePushAllowlistUsernames = types.ListNull(types.StringType)
+	model.ForcePushAllowlistTeams = types.ListNull(types.StringType)
+	model.BlockAdminMergeOverride = types.BoolNull()
+	model.EnableForcePush = types.BoolNull()
+	model.EnableForcePushAllowlist = types.BoolNull()
+	model.ForcePushAllowlistDeployKeys = types.BoolNull()
+	model.IgnoreStaleApprovals = types.BoolNull()
+	model.Priority = types.Int64Null()
 }
